6788c5afe93e51950743a3c569c67c04d1c8bb0f
Khaled021103 <khaledhesham007@gmail.com>
2023-07-15 18:20:01 +0300

    Update task6.php

--- 8-String Functions 53 to 62/task6.php
+++ 8-String Functions 53 to 62/task6.php
@@ -5,13 +5,3 @@ echo implode($chars);
 
 // Output
 //"Elzero"
-
-echo "<br>";
-// 7
-
-$chars = ["E", 1, 2, "l", "z", "E", "R", "o"];
-
-echo ucfirst(strtolower(substr_replace(implode($chars), "", 1, 2)));
-
-// Output
-//"Elzero"
