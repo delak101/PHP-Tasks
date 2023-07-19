83074b503801305e5a81474d9f9c34f2b9dcb9ec
Khaled Hesham <50780687+delak101@users.noreply.github.com>
2023-07-20 00:00:45 +0300

    Update task10.php

--- 10-Math Functions and Filters 73 to 81/task10.php
+++ 10-Math Functions and Filters 73 to 81/task10.php
@@ -1,14 +1,5 @@
 <?php
 
-// task 3
-
-$num1 = 11.5; // 11
-$num2 = 9.4898; // 9.5
-$num3 = -7.5; // -7
-
-echo round($num1, 0, PHP_ROUND_HALF_DOWN) . "<br>";
-echo round($num2, 1) . "<br>";
-echo round($num3, 0, PHP_ROUND_HALF_DOWN) . "<br>";
 
 // task 4
 $filter_name = [];
