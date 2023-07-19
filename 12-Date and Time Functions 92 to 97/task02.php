cf37e216cca81115d94408ad972132d7856e7252
Khaled Hesham <50780687+delak101@users.noreply.github.com>
2023-07-20 00:20:22 +0300

    Update task02.php

--- 12-Date and Time Functions 92 to 97/task02.php
+++ 12-Date and Time Functions 92 to 97/task02.php
@@ -3,10 +3,11 @@
 $date = "2005-10-02";
 $d = date_create("$date");
 
-date_default_timezone_set("Africa/Cairo");
-echo date_default_timezone_get() . "<br>";
-date_modify($d, "+15 hours, +15 minutes, +15 seconds");
-echo date_format($d, "Y, F, l 'dS' H:i:s") . "<br>";
+date_default_timezone_set("Africa/Cairo"); //setting timezone
+echo date_default_timezone_get() . "<br>"; // "Africa/Cairo"
+
+date_modify($d, "+15 hours, +15 minutes, +15 seconds"); // adding 15 hours & minutes & seconds
+echo date_format($d, "Y, F, l 'dS' H:i:s") . "<br>"; // "2005, October, Sunday '02nd' 15:15:15"
 
 // Output Needed
 // "Africa/Cairo"
