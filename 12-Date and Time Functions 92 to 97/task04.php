2a8149a833ffc14e047b0d8987ee55368c719848
Khaled Hesham <50780687+delak101@users.noreply.github.com>
2023-07-20 00:21:21 +0300

    Update task04.php

--- 12-Date and Time Functions 92 to 97/task04.php
+++ 12-Date and Time Functions 92 to 97/task04.php
@@ -1,13 +1,18 @@
 <?php
 
 date_default_timezone_set("Africa/Cairo");
+
 $date = "1990-10-01";
 $d=date_create($date);
+
 $now= date_create("now");
-$diff = date_diff($d, $now);
+
+$diff = date_diff($d, $now); // difference between dates
+
 echo "<pre>";
 print_r($diff);
 echo "</pre>";
+
 echo " From Epoch Time Till 1990-10-01 Is Approximately " . $diff->days . " Days<br>";
 echo " From Epoch Time Till 1990-10-01 Is Approximately " . $diff->y . " Years<br>";
 
