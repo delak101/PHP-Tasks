d0155adf103883a6743687407f3822122d5597ae
Khaled Hesham <50780687+delak101@users.noreply.github.com>
2023-07-20 00:01:50 +0300

    Update task10.php

--- 10-Math Functions and Filters 73 to 81/task10.php
+++ 10-Math Functions and Filters 73 to 81/task10.php
@@ -1,43 +1,5 @@
 <?php
 
-
-// task 4
-$filter_name = [];
-$filter_id = [];
-
-for ($i = 0; $i < count(filter_list()); $i++){
-    array_push($filter_name, filter_list()[$i]);
-    array_push($filter_id, filter_id(filter_list()[$i]));
-}
-
-echo "<pre>";
-print_r(array_combine($filter_id, $filter_name));
-echo "</pre>";
-// Array
-// (
-//   [257] => int
-//   [258] => boolean
-//   [259] => float
-//   [272] => validate_regexp
-//   [277] => validate_domain
-//   [273] => validate_url
-//   [274] => validate_email
-//   [275] => validate_ip
-//   [276] => validate_mac
-//   [513] => stripped
-//   [514] => encoded
-//   [515] => special_chars
-//   [522] => full_special_chars
-//   [516] => unsafe_raw
-//   [517] => email
-//   [518] => url
-//   [519] => number_int
-//   [520] => number_float
-//   [523] => add_slashes
-//   [1024] => callback
-// )
-
-
 // task 5
 $url1 = "http://www.elz ero.org";
 $url2 = "http://¥elzero.org";
