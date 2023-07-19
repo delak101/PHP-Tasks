a9fa62d48f6758a62d29d27b01123b9c3fd6853c
Khaled Hesham <50780687+delak101@users.noreply.github.com>
2023-07-20 00:01:16 +0300

    Create task3.php

---
+++ 10-Math Functions and Filters 73 to 81/task3.php
@@ -0,0 +1,9 @@
+<?php
+
+$num1 = 11.5; // 11
+$num2 = 9.4898; // 9.5
+$num3 = -7.5; // -7
+
+echo round($num1, 0, PHP_ROUND_HALF_DOWN) . "<br>";
+echo round($num2, 1) . "<br>";
+echo round($num3, 0, PHP_ROUND_HALF_DOWN) . "<br>";
